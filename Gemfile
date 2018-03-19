ad41ad8f83f675095a1e3a91aed2d219842f97d4
cmestera <ciprian.mester@aurea.com>
2018-03-19 19:32:14 +0200

    Add rcov format for ruby metrics plugin

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'rspec', :require => false, :group => :test
 gem 'simplecov', :require => false, :group => :test
+gem 'simplecov-rcov', :require => false, :group => :test
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 require 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::RcovFormatter
+]
+
 SimpleCov.start
 
 require_relative '../bin/string_ops'
